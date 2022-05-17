5921b14acc2b4845f1f8ff85b4beab3584efe2c1
Chris Maree <christopher.maree@gmail.com>
2022-05-17 10:03:19 +0200

    Restore AcrossToken.Parameterization.ts

--- test/AcrossToken.Parameterization.ts
+++ test/AcrossToken.Parameterization.ts
@@ -1,27 +1,27 @@
 import { expect, ethers, Contract, SignerWithAddress, toWei } from "./utils";
 import { acceleratingDistributorFixture } from "./AcceleratingDistributor.Fixture";
 
-let rewardToken: Contract, owner: SignerWithAddress, rando: SignerWithAddress;
+let acrossToken: Contract, owner: SignerWithAddress, rando: SignerWithAddress;
 
-describe("rewardToken: Parameterization", async function () {
+describe("AcrossToken: Parameterization", async function () {
   beforeEach(async function () {
     [owner, rando] = await ethers.getSigners();
-    ({ rewardToken } = await acceleratingDistributorFixture());
+    ({ acrossToken } = await acceleratingDistributorFixture());
   });
   it("Token config set correctly", async function () {
-    expect(await rewardToken.name()).to.equal("Across Protocol Token");
-    expect(await rewardToken.symbol()).to.equal("ACX");
-    expect(await rewardToken.decimals()).to.equal(18);
-    expect(await rewardToken.owner()).to.equal(owner.address);
+    expect(await acrossToken.name()).to.equal("Across Protocol Token");
+    expect(await acrossToken.symbol()).to.equal("ACX");
+    expect(await acrossToken.decimals()).to.equal(18);
+    expect(await acrossToken.owner()).to.equal(owner.address);
   });
   it("Token mint permissions works as expected set correctly", async function () {
     // Owner can mint and burn.
-    await expect(() => rewardToken.mint(owner.address, toWei(69))).to.changeTokenBalance(rewardToken, owner, toWei(69));
-    await expect(() => rewardToken.burn(owner.address, toWei(4))).to.changeTokenBalance(rewardToken, owner, toWei(-4));
+    await expect(() => acrossToken.mint(owner.address, toWei(69))).to.changeTokenBalance(acrossToken, owner, toWei(69));
+    await expect(() => acrossToken.burn(owner.address, toWei(4))).to.changeTokenBalance(acrossToken, owner, toWei(-4));
 
     // Non-owner cant call either method.
     const revertMsg = "Ownable: caller is not the owner";
-    await expect(rewardToken.connect(rando).mint(rando.address, 420)).to.be.revertedWith(revertMsg);
-    await expect(rewardToken.connect(rando).burn(rando.address, 420)).to.be.revertedWith(revertMsg);
+    await expect(acrossToken.connect(rando).mint(rando.address, 420)).to.be.revertedWith(revertMsg);
+    await expect(acrossToken.connect(rando).burn(rando.address, 420)).to.be.revertedWith(revertMsg);
   });
 });
