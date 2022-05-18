8ddb290ebf1cef35ecd6298d7d3a6bc83ae0de59
Chris Maree <christopher.maree@gmail.com>
2022-05-18 09:48:38 +0200

    fix n07: addressed typographical errors (#17)

--- test/AcceleratingDistributor.RewardAccumulation.ts
+++ test/AcceleratingDistributor.RewardAccumulation.ts
@@ -51,7 +51,7 @@ describe("AcceleratingDistributor: Staking Rewards", async function () {
     // Rewards entitled to the user should now be duration * baseEmissionRate * multiplier as 1000 * 0.01 * 5 = 50
     expect(await distributor.getOutstandingRewards(lpToken1.address, depositor1.address)).to.equal(toWei(50));
   });
-  it("Multiple depositors, pro-rate distribution: same stake time and claim time", async function () {
+  it("Multiple depositors, pro-rata distribution: same stake time and claim time", async function () {
     // Create a simple situation wherein both depositors deposit at the same time but have varying amounts.
     await distributor.connect(depositor1).stake(lpToken1.address, stakeAmount); // stake 10.
     await distributor.connect(depositor2).stake(lpToken1.address, stakeAmount.mul(3)); // stake 30.
@@ -69,7 +69,7 @@ describe("AcceleratingDistributor: Staking Rewards", async function () {
       // Get the rewards. Check the cash flows are as expected. The distributor should send 2.7 to the depositor2.
       .to.changeTokenBalances(acrossToken, [distributor, depositor2], [toWei(-2.7), toWei(2.7)]);
   });
-  it("Multiple depositors, pro-rate distribution: same stake time and separate claim time", async function () {
+  it("Multiple depositors, pro-rata distribution: same stake time and separate claim time", async function () {
     await distributor.connect(depositor1).stake(lpToken1.address, stakeAmount); // stake 10.
     await distributor.connect(depositor2).stake(lpToken1.address, stakeAmount.mul(3)); // stake 30.
     // Try claiming from one user halfway through some staking period and validating that multipliers are treated independently.
@@ -100,9 +100,9 @@ describe("AcceleratingDistributor: Staking Rewards", async function () {
       // Get the rewards. Check the cash flows are as expected. The distributor should send 4.49 to the depositor2.
       .to.changeTokenBalances(acrossToken, [distributor, depositor2], [toWei(-4.95), toWei(4.95)]);
   });
-  it("Multiple depositors, pro-rate distribution: separate stake time and separate claim time", async function () {
+  it("Multiple depositors, pro-rata distribution: separate stake time and separate claim time", async function () {
     // Stake with one user, advance time, then stake with another use. We should be able to track the evaluation of the
-    // pro-rate rewards, as expected.
+    // pro-rata rewards, as expected.
 
     await distributor.connect(depositor1).stake(lpToken1.address, stakeAmount); // stake 10.
     await advanceTime(timer, 200);
@@ -117,10 +117,10 @@ describe("AcceleratingDistributor: Staking Rewards", async function () {
     // Advance time another 200 seconds.
     await advanceTime(timer, 200);
 
-    // Now, the depositor1 should be entitled to their first amount + the pro-rate rewards for the second period as
+    // Now, the depositor1 should be entitled to their first amount + the pro-rata rewards for the second period as
     // 400 * 0.01 * (1 + 400 / 1000 * (5 - 1)) * (1 / 4 + 1) / 2 = 6.5. This equation can be though as attributing the
     // full period plus the multiplier growing over the full period * (1 / 4 + 1) / 2 which represents that for the first
-    // 200 seconds the pro-rate decision is 1 (i.e (1)/2) and for the second 200 seconds it is 1/4 (i.e (1/4)/2).
+    // 200 seconds the pro-rata decision is 1 (i.e (1)/2) and for the second 200 seconds it is 1/4 (i.e (1/4)/2).
     expect(await distributor.getOutstandingRewards(lpToken1.address, depositor1.address)).to.equal(toWei(6.5));
 
     // The depositor2 balance should simply be their prop-rate share of the distribution drawn over the 200 seconds as
