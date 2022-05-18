686f5a0006a296088eacc346e0a85d98194e1fed
Chris Maree <christopher.maree@gmail.com>
2022-05-18 13:18:33 +0200

    fix m01: addition of more input validation (#6)

--- test/AcceleratingDistributor.Admin.ts
+++ test/AcceleratingDistributor.Admin.ts
@@ -79,6 +79,17 @@ describe("AcceleratingDistributor: Admin Functions", async function () {
       )
     ).to.be.revertedWith("Staked token is reward token");
   });
+  it("Cannot set bad staking configs", async function () {
+    await expect(
+      distributor.enableStaking(lpToken1.address, true, baseEmissionRate, toWei(toWei(1)), secondsToMaxMultiplier)
+    ).to.be.revertedWith("maxMultiplier can not be set too large");
+    await expect(
+      distributor.enableStaking(lpToken1.address, true, baseEmissionRate, maxMultiplier, 0)
+    ).to.be.revertedWith("secondsToMaxMultiplier must be greater than 0");
+    await expect(
+      distributor.enableStaking(lpToken1.address, true, toWei(10000000000), maxMultiplier, secondsToMaxMultiplier)
+    ).to.be.revertedWith("baseEmissionRate can not be set too large");
+  });
 
   it("Non owner cant execute admin functions", async function () {
     await expect(distributor.connect(rando).configureStakingToken(lpToken1.address, true, 4, 2, 0)).to.be.revertedWith(
