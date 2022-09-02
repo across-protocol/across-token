ab67983fdeb6781b9dbcc6479bff46d090dcba47
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-02 15:09:29 -0400

    nit: update comment content

--- test-utils.ts
+++ test-utils.ts
@@ -1,5 +1,5 @@
 // Note: this file sits on a separate export path and is intended to export test utilities and code:
-// You can import it like this: import * as testUtils from "@across-protocol/contracts-v2/dist/test-utils".
+// You can import it like this: import * as testUtils from "@across-protocol/across-token/dist/test-utils".
 // This is separated because this code assumes the caller has a hardhat config because it imports
 // hardhat. For non-test code, import the standard index file:
 // import * as across-token from "@across-protocol/across-token"
