b814db07d22c7d489fe8f961e37f4048e2f4486f
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-02 15:02:56 -0400

    fix: revert changes that do not contribute

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,7 @@ module.exports = {
     node: true,
   },
   plugins: ["@typescript-eslint", "mocha"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-  ],
+  extends: ["standard", "plugin:prettier/recommended", "plugin:node/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
@@ -22,9 +15,5 @@ module.exports = {
     "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }],
     "mocha/no-exclusive-tests": "error",
     "@typescript-eslint/no-var-requires": 0,
-  },
-  node: {
-    resolvePaths: ["node_modules/@types"],
-    tryExtensions: [".js", ".json", ".node", ".ts", ".d.ts"],
   },
 };
